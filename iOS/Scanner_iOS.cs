7c9a819f27e751772558f2e04b130fcab2643f2d
海ねこ <umicho.net@gmail.com>
2014-11-02 23:11:46 +0900

    Initial commit.

---
+++ iOS/Scanner_iOS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using ZXing.Mobile;
+
+[assembly: Xamarin.Forms.Dependency (typeof (ZXingNetMobileSample.iOS.Scanner_iOS))]
+
+namespace ZXingNetMobileSample.iOS
+{
+    public class Scanner_iOS : IScanner
+    {
+        public async Task<string> ScanAsync()
+        {
+            var scanner = new MobileBarcodeScanner();
+            var result = await scanner.Scan();
+
+            if (result != null)
+            {
+                return result.Text;
+            }
+            return string.Empty;
+        }
+    }
+}
+
